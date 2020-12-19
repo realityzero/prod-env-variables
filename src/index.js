6d813e258d589d543d23525395e68fd05f62fae4
arpit o.O <arpitremarkable@gmail.com>
2020-12-19 14:52:08 +0530

    Fix attempt

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const keys = keysInput.split(/\r?\n/);
 console.log("evaluating", keys);
 
 for (let key of keys) {
-	if (isProd) {
+	if (isProd === true) {
 		console.log("checking for key", `${key}_PROD`);
 		const value = process.env[`${key}_PROD`];
 		console.log(`replacing ${key} with value from ${key}_PROD`);
